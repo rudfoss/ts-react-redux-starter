6df371ee25535a794aac10f5ccfd25f9d73c882c
Thomas Rudfoss <thomas.rudfoss@gmail.com>
2019-09-09 07:09:11 +0200

    Improved doc

--- src/utils/codeSplitHelpers.ts
+++ src/utils/codeSplitHelpers.ts
@@ -1,10 +1,20 @@
-import { lazy } from "react"
+import { ComponentType, lazy } from "react"
+import { IDuckExport } from "../interfaces"
 import { StoreManager } from "./StoreManager"
 
+/**
+ * This function helps define code splitting boundaries and ensures imported modules also
+ * register their reducers and sagas.
+ *
+ * Example:
+ * `importChunk(storeManager)(() => import(/* webpackChunkName. "chunkName" *\/""./Component"))`
+ *
+ * @param storeManager
+ */
 export const importChunk =
-	(storeManager: StoreManager) =>
-	(importer: any) =>
-	lazy(async () => {
+	<TComponent extends ComponentType<any>>(storeManager: StoreManager) =>
+	(importer: (() => Promise<{ default: TComponent, duck?: IDuckExport }>)) =>
+	lazy<TComponent>(async () => {
 		const component = await importer()
 		if (component.duck) {
 			storeManager.addDucks(component.duck)
